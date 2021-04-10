860f9f3703d06a20a179226f7b8585a9975e9f03
Dio-dev1 <VictorySupper@gmail.com>
2021-04-11 00:31:20 +0800

    video live stream

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const rtc = {
+  // For the local client
+  client: null,
+  // For the local audio and video tracks
+  localAudioTrack: null,
+  localVideoTrack: null,
+};
+
+export const options = {
+  // Pass your app ID here
+  appId: "98917f5cc3ea4f6f9a4c1736cb5ab93a",
+  // Pass a token if your project enables the App Certificate
+  token: '00698917f5cc3ea4f6f9a4c1736cb5ab93aIACzX6bI116A7tv3ceMv8dTmC0y/YHkPFqrrdZsbHlXfz3tZ+mIAAAAAEAAVx5nnfOByYAEAAQB84HJg',
+};
